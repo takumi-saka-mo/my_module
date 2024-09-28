18477e053f59eac946db7a55027220dad86cb440
takumi.saka.mo <takumi.saka.mo0107@gmail.com>
2024-09-29 04:16:40 +0900

    setup update

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ## Installation
+---
+
 
 ## Usage
 ```python
@@ -8,3 +10,5 @@ tickers = ["^N225", "AAPL", "GOOGL"]
 company_list = ["Nikkei", "Apple", "Google"]
 start = "2020-01-01"
 end = "2021-01-01"
+
+beta(tickers, company_list, start, end)
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="myawsname",
+    name="my_finance_package",
     version="0.0.1",
     author="Takumi Sakamoto",
     author_email="takumi.saka.mo0107@gmail.com",
