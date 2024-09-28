4bacf95168f0ff45595aaed5c47b3cc86ccc970d
takumi.saka.mo <takumi.saka.mo0107@gmail.com>
2024-09-29 04:40:14 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="my_finance_package",  # パッケージの名前を変更
+    name="my_finance_package",  # パッケージの名前
     version="0.0.1",
     author="Takumi Sakamoto",
     author_email="takumi.saka.mo0107@gmail.com",
@@ -18,7 +18,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    packages=['my_finance'],  # 必要なパッケージを明示的に指定
+    packages=['my_finance'],  # my_finance パッケージを指定
     python_requires=">=3.7",
     install_requires=[
         "pandas",
